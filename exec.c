db0804173efebd89732dfe0a00ebae110c337ac5
Von-sledge <papaaamoah568@gmail.com>
2023-12-18 03:02:49 +0000

    Monty_project - Stacks and queues

---
+++ exec.c
@@ -0,0 +1,60 @@
+#include "monty.h"
+
+/**
+ * execute - executes the opcode
+ * @stack: head linked list - stack
+ * @counter: line_counter
+ * @file: pointer monty to monty file
+ * @content: line content
+ * Return: 0
+ */
+
+int execute(char *content stack_t **stack, unsigned int counter, FILE *file)
+{
+	instruction_t opst[] = {
+				{"push", f_push},
+				{"pall", f_pall},
+				{"pint", f_pint},
+				{"pop", f_pop},
+				{"swap", f_swap},
+				{"add", f_add},
+				{"nop", f_nop},
+				{"sub", f_sub},
+				{"div", f_div},
+				{"mult", f_mult},
+				{"mod", f_mod},
+				{"pchar", f_pchar},
+				{"pstr", f_pstr},
+				{"rot1", f_rot1},
+				{"rotr", f_rotr},
+				{"queue", f_queue},
+				{"stack", f_stack},
+				{NULL, NULL}
+				};
+	unsigned int i = 0;
+	char *op;
+
+	op = strtok(content, " \n\t");
+	if(op && op[0] == "#')
+		return (0);
+	bus.arg = strtok(NULL," \n\t");
+	while (opst[i].opcode && op)
+	{
+		if(strcomp(op, opst[i].opcode) == 0)
+		{
+			opst[i].f(stack, counter);
+			return(0);
+		}
+		i++;
+	}
+
+	if(op && opst[i].opcode == NULL)
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", counter, op)
+			fclose(file);
+			free(content);
+			free_stack(*stack);
+			exit(EXIT_FAILURE);
+	}
+	return (1);
+}
---
+++ pstr.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * f_pstr - prints the string starting at the top 
+ * of the stack, followed by a new line
+ * @head: stack head
+ * @counter: line_number
+ * Return: 0;
+ */
+
+void f_stack(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	while(h)
+	{
+		if(h->n > 127 || h-> <= 0)
+		{
+			break;
+		}
+
+		printf("%c", h->n);
+		h = h->next;
+	}
+	printf("\n");
+}
+
